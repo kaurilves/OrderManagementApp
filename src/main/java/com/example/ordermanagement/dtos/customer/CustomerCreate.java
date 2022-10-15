65683a905e2299e611d8ec8cfe5590dd85a4ca76
kaurilves <kaurilves@gmail.com>
2022-10-15 14:27:54 +0300

    Order managemenet - done some fixes, WIP

--- src/main/java/com/example/ordermanagement/dtos/customer/CustomerCreate.java
+++ src/main/java/com/example/ordermanagement/dtos/customer/CustomerCreate.java
@@ -2,14 +2,27 @@ package com.example.ordermanagement.dtos.customer;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class CustomerCreate implements Serializable {
 
+    @NotNull
+    @NotBlank
     private String fullName;
+
+    @NotNull
+    @NotBlank
     private String registrationCode;
+
+    @NotNull
+    @NotBlank
     private String email;
+
+    @NotNull
+    @NotBlank
     private String telephone;
 
 }
